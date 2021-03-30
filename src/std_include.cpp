5060a1318083e51f742a11b33d90494e47572191
Maurice Heumann <maurice.heumann@wibu.com>
2021-03-30 15:44:27 +0200

    Formatting fix

--- src/std_include.cpp
+++ src/std_include.cpp
@@ -2,10 +2,10 @@
 
 extern "C"
 {
-int s_read_arc4random(void*, size_t)
-{
-	return -1;
-}
+	int s_read_arc4random(void*, size_t)
+	{
+		return -1;
+	}
 
 #if !defined(__linux__)
 	int s_read_getrandom(void*, size_t)
