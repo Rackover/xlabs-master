e4aa0f4638140018492da16c2601281b549615e2
rackover <roxanne@thegamebakers.com>
2022-08-18 20:08:39 +0200

    Addressed snake's review

--- src/services/kill_list.cpp
+++ src/services/kill_list.cpp
@@ -23,21 +23,17 @@ bool kill_list::contains(const network::address& address, std::string& reason)
 
 void kill_list::add_to_kill_list(kill_list::kill_list_entry add)
 {
-	bool any_change = false;
-	entries_container.access([&add, &any_change](kill_list_entries& entries)
+	bool any_change = entries_container.access<bool>([&add, &any_change](kill_list_entries& entries)
 		{
 			auto existing_entry = entries.find(add.ip_address);
 			if (existing_entry == entries.end() || existing_entry->second.reason != add.reason)
 			{
-				if (existing_entry != entries.end())
-				{
-					entries.erase(existing_entry);
-				}
-
-				entries.emplace(add.ip_address, std::move(add));
+				entries[add.ip_address] = std::move(add);
 				console::info("Added %s to kill list (reason: %s)", add.ip_address.data(), add.reason.data());
-				any_change = true;
+				return true;
 			}
+
+			return false;
 		});
 
 	if (any_change)
@@ -57,14 +53,15 @@ void kill_list::remove_from_kill_list(const network::address& remove)
 
 void kill_list::remove_from_kill_list(const std::string& remove)
 {
-	bool any_change = false;
-	entries_container.access([&remove, &any_change](kill_list_entries& entries)
+	bool any_change = entries_container.access<bool>([&remove, &any_change](kill_list_entries& entries)
 	{
 		if (entries.erase(remove))
 		{
 			console::info("Removed %s from kill list", remove.data());
-			any_change = true;
+			return true;
 		}
+
+		return false;
 	});
 
 	if (any_change)
@@ -79,58 +76,59 @@ void kill_list::remove_from_kill_list(const std::string& remove)
 
 void kill_list::reload_from_disk()
 {
-	if (!utils::io::file_exists(kill_file))
+	std::string contents;
+	
+	if (utils::io::read_file(kill_file, &contents))
+	{
+		std::istringstream string_stream(contents);
+		std::string line;
+
+		entries_container.access([&string_stream, &line, this](kill_list_entries& entries)
+			{
+				entries.clear();
+				while (std::getline(string_stream, line))
+				{
+					if (line[0] == '#')
+					{
+						// comments or ignored line
+						continue;
+					}
+
+					std::string ip;
+					std::string comment;
+
+					auto index = line.find(' ');
+					if (line.find(' ') != std::string::npos)
+					{
+						ip = line.substr(0, index);
+						comment = line.substr(index + 1);
+					}
+					else
+					{
+						ip = line;
+					}
+
+					if (ip.empty())
+					{
+						continue;
+					}
+
+					// Double line breaks from windows' \r\n
+					if (ip[ip.size() - 1] == '\r')
+					{
+						ip.pop_back();
+					}
+
+					entries.emplace(ip, kill_list::kill_list_entry(ip, comment));
+				}
+
+				console::info("Loaded %i kill list entries from %s", entries.size(), kill_file.data());
+			});
+	}
+	else 
 	{
 		console::info("Could not find %s, no kill list will be loaded.", kill_file.data());
-		return;
 	}
-
-	std::string contents = utils::io::read_file(kill_file);
-
-	std::istringstream string_stream(contents);
-	std::string line;
-
-	entries_container.access([&string_stream, &line, this](kill_list_entries& entries)
-	{
-		entries.clear();
-		while (std::getline(string_stream, line))
-		{
-			if (line[0] == '#')
-			{
-				// comments or ignored line
-				continue;
-			}
-
-			std::string ip;
-			std::string comment;
-
-			auto index = line.find(' ');
-			if (line.find(' ') != std::string::npos)
-			{
-				ip = line.substr(0, index);
-				comment = line.substr(index + 1);
-			}
-			else
-			{
-				ip = line;
-			}
-
-			if (ip.empty())
-			{
-				continue;
-			}
-
-			// Double line breaks from windows' \r\n
-			if (ip[ip.size() - 1] == '\r')
-			{
-				ip.pop_back();
-			}
-
-			entries.emplace(ip, kill_list::kill_list_entry(ip, comment));
-		}
-
-		console::info("Loaded %i kill list entries from %s", entries.size(), kill_file.data());
-	});
 }
 
 void kill_list::write_to_disk()
